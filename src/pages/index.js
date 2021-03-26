3abb50fa53dcc753df5f9cd020e4d84905ea469c
vamseedhar26 <40071293+vamseedhar26@users.noreply.github.com>
2021-03-27 01:04:35 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export default function Index() {
   return <h1>Hello there, My dear viewer, Welcome to my webpage</h1>;
+    return <h2>Hope you like the webpage here, I know it is blank as of now.</h2>;
+
 }
